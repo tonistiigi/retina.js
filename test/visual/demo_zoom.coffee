74586ef75af00bce059310b04794df5de83d1650
Tõnis Tiigi <tonis@digitalfruit.info>
2011-09-05 21:37:21 +0300

    Dummy for visual tests/demos

---
+++ test/visual/demo_zoom.coffee
@@ -0,0 +1,4 @@
+console.log 'Running visual test'
+
+window.addEventListener "load", ->
+    console.log "Loaded", retina
---
+++ test/visual/demo_zoom.js
@@ -0,0 +1,6 @@
+(function() {
+  console.log('Running visual test');
+  window.addEventListener("load", function() {
+    return console.log("Loaded", retina);
+  });
+}).call(this);
